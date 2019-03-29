0264bcb4ff801161285ddb372be6b436a3cdef1a
Andrew Leonardi <ndrewleonardi2@users.noreply.github.com>
2019-03-29 10:02:48 -0700

    [saved] connected component to redux store

--- screens/SavedScreen.js
+++ screens/SavedScreen.js
@@ -1,27 +1,32 @@
-import React from 'react';
-import { Image, ScrollView, StyleSheet, Text, View } from 'react-native';
-import data from '../data.js';
+import React, { Component } from 'react';
+import {
+  Image,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+import ACTION_CREATORS from '../redux/action_creators';
+import { connect } from 'react-redux';
 
-export default class SavedScreen extends React.Component {
+class SavedScreen extends Component {
   static navigationOptions = {
     title: 'Saved',
   };
-
   render() {
     return (
       <ScrollView style={styles.container}>
         {
-          data.map((pet) => {
+          this.props.saved.map((pet) => {
             return <View style={styles.card} key={pet.id}>
               <Image
                 style={styles.image}
                 source={{ uri: pet.img }}
               />
               <View style={styles.description}>
-                <Text>{`Name: ${pet.name}, Age: ${pet.age}, Gender: ${pet.sex}`}</Text>
+                <Text style={styles.title}>{`${pet.name}, ${pet.age} yr(s), ${pet.sex}`}</Text>
                 <Text>{pet.profile}</Text>
               </View>
-
             </View>
           })
         }
@@ -51,4 +56,23 @@ const styles = StyleSheet.create({
   description: {
     flex: 5
   },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold'
+  }
 });
+
+const mapStateToProps = (state) => {
+  console.log('Mapping state to props', state);
+  return {
+    saved: state.saved
+  };
+}
+
+const mapActionsToProps = (dispatch) => ({
+  save(pet) {
+    return dispatch(ACTION_CREATORS.save_pet(pet));
+  }
+});
+
+export default connect(mapStateToProps, mapActionsToProps)(SavedScreen);
