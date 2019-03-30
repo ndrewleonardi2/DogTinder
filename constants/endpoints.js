ef7a537b6378f6c8d080b2f501d52fb26009791c
Andrew Leonardi <ndrewleonardi2@users.noreply.github.com>
2019-03-29 17:19:58 -0700

    [chore] modifed enpoints file

--- constants/endpoints.js
+++ constants/endpoints.js
@@ -1,2 +1,7 @@
-export const PETS = 'https://s3-us-west-2.amazonaws.com/cozi-interview-dev/pets.json';
-export const PROFILE = 'https://s3-us-west-2.amazonaws.com/cozi-interview-dev/settings.json';
+const PETS = 'https://s3-us-west-2.amazonaws.com/cozi-interview-dev/pets.json';
+const PROFILE = 'https://s3-us-west-2.amazonaws.com/cozi-interview-dev/settings.json';
+
+export default {
+  PETS,
+  PROFILE
+}
